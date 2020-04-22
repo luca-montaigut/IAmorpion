4f82fb812c38c63cf9e548ad53981fb942b026d4
Matthieu BRAULT <matthieu.brault@epitech.eu>
2020-04-22 14:18:15 +0200

    refactor to class

--- morpion.js
+++ morpion.js
@@ -1,85 +1,89 @@
-ia = "J2";
-map = [];
-for (i = 0; i < 3; i++) {
-	map[i] = [];
-	for (j = 0; j < 3; j++)
-		map[i][j] = "EMPTY";
-}
-finish = false;
-
-const getZone = (x, y) => {
-	if (y == 0)
-		return 'A' + (x + 1);
-	else if (y == 1)
-		return 'B' + (x + 1);
-	else
-		return 'C' + (x + 1);
-}
-
-const fillGrid = (x, y, player) => {
-	const image = (player == joueur) ? 'croix' : 'rond';
-	const zone = getZone(x, y);
-
-	if (map[x][y] != "EMPTY")
-		return false;
-	map[x][y] = player;
-	document.getElementById(zone).style.backgroundImage = `url(image-morpion/${image}.png)`;
-	document.getElementById(zone).className += " filled";
-	checking(player);
-	return true;
-}
-
-const playerTurn = (x, y) => {
-	if (finish)
-		return;
-	if (!fillGrid(x, y, joueur)) {
-		alert('La case n\'est pas vide');
-		return;
+class Morpion {
+	constructor(player) {
+		this.player = player;
+		this.ia = (player == "J1") ? "J2" : "J1";
+		this.map = [];
+		for (let i = 0; i < 3; i++) {
+			this.map[i] = [];
+			for (let j = 0; j < 3; j++) {
+				this.map[i][j] = "EMPTY";
+				document.getElementById(this.getZone(i, j)).onclick = () => this.playerTurn(i, j);
+			}
+		}
+		this.finish = false;
+		if (this.ia === "J1")
+			this.iaTurn();
 	}
-	else if (!finish)
-		iaTurn();
-};
 
-const iaTurn = () => {
-	x = 0;
-	y = 0;
-	while (!fillGrid(x, y, ia)) {
-		x++;
-		if (x >= 3) {
-			x = 0;
-			y++;
+	getZone = (x, y) => {
+		if (y == 0)
+			return 'A' + (x + 1);
+		else if (y == 1)
+			return 'B' + (x + 1);
+		else
+			return 'C' + (x + 1);
+	}
+
+	fillGrid = (x, y, player) => {
+		const image = (player == this.player) ? 'croix' : 'rond';
+		const zone = this.getZone(x, y);
+
+		if (this.map[x][y] != "EMPTY")
+			return false;
+		this.map[x][y] = player;
+		document.getElementById(zone).style.backgroundImage = `url(image-morpion/${image}.png)`;
+		document.getElementById(zone).className += " filled";
+		this.checking(player);
+		return true;
+	}
+
+	checking = (player) => {
+		const one = this.map[0][0];
+		const two = this.map[0][1];
+		const three = this.map[0][2];
+		const four = this.map[1][0];
+		const five = this.map[1][1];
+		const six = this.map[1][2];
+		const seven = this.map[2][0];
+		const eight = this.map[2][1];
+		const nine = this.map[2][2];
+		if (one === two && one === three && one != "EMPTY" ||
+			four === five && four === six && four != "EMPTY" ||
+			seven === eight && seven === nine && seven != "EMPTY" ||
+			one === five && one === nine && one != "EMPTY" ||
+			three === five && three === seven && three != "EMPTY" ||
+			one === four && one === seven && one != "EMPTY" ||
+			two === five && two === eight && two != "EMPTY" ||
+			three === six && three === nine && three != "EMPTY") {
+			this.finish = true;
+			if (player == this.ia) {
+				document.getElementById('win').textContent = 'L\'IA a gagné !';
+			} else if (player == this.player) {
+				document.getElementById('win').textContent = 'Tu as battu l\'IA !';
+			}
+		}
+	}
+
+	playerTurn = (x, y) => {
+		if (this.finish)
+			return;
+		if (!this.fillGrid(x, y, this.player))
+			return alert('La case n\'est pas vide');
+		else if (!this.finish)
+			this.iaTurn();
+	}
+
+	iaTurn = () => {
+		let x = 0;
+		let y = 0;
+		while (!this.fillGrid(x, y, this.ia)) {
+			x++;
+			if (x >= 3) {
+				x = 0;
+				y++;
+			}
 		}
 	}
 }
 
-const checking = (player) => {
-	one = map[0][0];
-	two = map[0][1];
-	three = map[0][2];
-	four = map[1][0];
-	five = map[1][1];
-	six = map[1][2];
-	seven = map[2][0];
-	eight = map[2][1];
-	nine = map[2][2];
-	if (one === two && one === three && one != "EMPTY" ||
-		four === five && four === six && four != "EMPTY" ||
-		seven === eight && seven === nine && seven != "EMPTY" ||
-		one === five && one === nine && one != "EMPTY" ||
-		three === five && three === seven && three != "EMPTY" ||
-		one === four && one === seven && one != "EMPTY" ||
-		two === five && two === eight && two != "EMPTY" ||
-		three === six && three === nine && three != "EMPTY") {
-		finish = true;
-		if (player == ia) {
-			document.getElementById('victoire').textContent = 'L\'IA a gagné !';
-		} else if (player == joueur) {
-			document.getElementById('victoire').textContent = 'Tu as battu l\'IA !';
-		}
-	}
-}
-
-joueur = (ia == "J1") ? "J2" : "J1";
-if (ia == "J1") {
-	iaTurn();
-}
+const morpion = new Morpion("J1");
